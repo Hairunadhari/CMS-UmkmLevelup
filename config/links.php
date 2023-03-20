ccb50075368a0e15da80367c9b858f8f8c1c6120
arifin.twospaces <arifin.twospaces@gmail.com>
2023-03-20 11:33:26 +0700

    update

---
+++ config/links.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'help_url' => 'testing-link',
+    'helpdesk_sitemap_url' => 'testing-link',
+    'changelog_url' => 'testing-link',
+    'github_url' => 'testing-link',
+    'github_forum_url' => 'testing-link',
+    'facebook_group' => 'testing-link',
+    'twitter' => 'testing-link',
+    'zapier_integration' => 'testing-link',
+    'book_onboarding' => 'testing-link',
+    'feature_requests' => 'testing-links'
+];
---
+++ resources/js/pages/community/students-academics-ngos.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="mt-6 flex flex-col">
+      <div class="w-full md:max-w-3xl md:mx-auto px-4 md:pt-16 pb-10">
+        <h1 class="sm:text-5xl mb-4">
+          #
+        </h1>
+        <notion-page page-id="c65a499d39834e0b8978556a8d7af867" />
+      </div>
+    </div>
+    <open-form-footer />
+  </div>
+</template>
+
+<script>
+import Footer from '../../components/pages/Footer.vue'
+
+export default {
+  components: { Footer },
+  layout: 'default',
+
+  props: {
+    metaTitle: { type: String, default: 'Form Quisioneer builder' },
+    metaDescription: { type: String, default: 'Description Here.....' }
+  },
+
+  data: () => ({
+  }),
+
+  computed: {},
+
+  mounted () {
+  }
+}
+</script>
