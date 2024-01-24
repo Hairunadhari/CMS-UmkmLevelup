2f5b191cdc1c3e4d7f182233ec8f93be309b2775
Yoli Alfitriani <hairunadhari@gmail.com>
2024-01-24 17:12:23 +0700

    update

--- resources/views/wilayah/kabupaten/edit.blade.php
+++ resources/views/wilayah/kabupaten/edit.blade.php
@@ -13,14 +13,17 @@
                 <form action="{{url('/update-kabupaten',$data->id_kabupaten)}}" method="post" enctype="multipart/form-data">
                     @csrf
                     @method('put')
-                    <select class="form-control select2" name="id_provinsi"  required>
-                        @foreach ($provinsi as $item)
-                       <option value="{{ $item->id_provinsi }}"
-                           {{ $item->id_provinsi == $data->id_provinsi ? 'selected' : '' }}>
-                           {{ $item->nama_provinsi }}
-                       </option>
-                       @endforeach
-                   </select>
+                    <div class="form-group">
+                        <label>Nama Kabupaten <span style="color: red">*</span></label>
+                        <select class="form-control select2" name="id_provinsi"  required>
+                            @foreach ($provinsi as $item)
+                           <option value="{{ $item->id_provinsi }}"
+                               {{ $item->id_provinsi == $data->id_provinsi ? 'selected' : '' }}>
+                               {{ $item->nama_provinsi }}
+                           </option>
+                           @endforeach
+                       </select>
+                  </div>
                         <div class="form-group">
                             <label>Nama Kabupaten <span style="color: red">*</span></label>
                             <input type="text" class="form-control" value="{{$data->nama_kabupaten}}" name="nama_kabupaten" required>
