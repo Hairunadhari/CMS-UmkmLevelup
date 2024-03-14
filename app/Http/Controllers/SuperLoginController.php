e5af5cf9264fae0b066f2eecac14df3726e6c2b9
unknown <hairunadhari@gmail.com>
2024-03-14 11:17:26 +0700

    update

--- app/Http/Controllers/SuperLoginController.php
+++ app/Http/Controllers/SuperLoginController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Session;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
 // use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\DB;
-use Session;
 
 class SuperLoginController extends Controller
 {
@@ -29,52 +29,70 @@ class SuperLoginController extends Controller
         return view('pages-umkm.auth-login');   
     }
 
-    public function submitLogin(Request $request)
-    {
-        // dd('test');
-        // if (Auth::check()) {
-        //     return redirect()->route('set-level');
-        // }
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
 
-        try {
-            if ($this->guard()->attempt($credentials)) {
-                $request->session()->regenerate();
+public function submitLogin(Request $request)
+{
+    $credentials = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ]);
 
-                // dd()
-                if ($this->guard()->user()->id_role == 2 or $this->guard()->user()->id_role == 3 or $this->guard()->user()->id_role == 4) {
-                    $request->session()->put('name', $this->guard()->user()->name);
-                    $request->session()->put('id_user', $this->guard()->user()->id);
-                    $request->session()->put('id_role', $this->guard()->user()->id_role);
-                    
-                    if($this->guard()->user()->id_role == 2){
-                        return redirect()->route('/dashboard'); 
-                    }else if ($this->guard()->user()->id_role == 4 or $this->guard()->user()->id_role == 3) {
-                        return redirect()->route('/kuesioner-unverif'); 
-                    }else{
-                        return redirect()->route('/list-materi'); 
+    try {
+        if ($this->guard()->attempt($credentials)) {
+            $request->session()->regenerate();
+
+            // Simpan URL sebelumnya jika ada
+            $previousUrl = Session::get('previousUrl');
+            // dd($previousUrl);
+
+            // Lakukan pengecekan role pengguna
+            if ($this->guard()->user()->id_role == 2 || $this->guard()->user()->id_role == 3 || $this->guard()->user()->id_role == 4) {
+                // Simpan data pengguna ke dalam session
+                $request->session()->put('name', $this->guard()->user()->name);
+                $request->session()->put('id_user', $this->guard()->user()->id);
+                $request->session()->put('id_role', $this->guard()->user()->id_role);
+
+                // Redirect sesuai role pengguna
+                if ($this->guard()->user()->id_role == 2) {
+                    if (!$previousUrl) {
+                        return redirect()->route('/dashboard');
+                    }else {
+                        return redirect($previousUrl);
                     }
-                }else{
-                    return redirect('login');
+                } elseif ($this->guard()->user()->id_role == 4 || $this->guard()->user()->id_role == 3) {
+                    if (!$previousUrl) {
+                        return redirect()->route('kuesioner-unverif');
+                    } else {
+                        return redirect($previousUrl);
+                    }
+                    
+                } else {
+                    return redirect()->route('list-materi');
                 }
-                
+            } else {
+                // Jika role tidak valid, redirect ke halaman login
+                return redirect('login');
             }
-        } catch (\Throwable $th) {
-            $request->session()->flash('alert', [
-                'type' => 'error',
-                'message' => 'Silahkan periksa kembali email password anda.',
-            ]);
         }
-        
-        return redirect('login');
+    } catch (\Throwable $th) {
+        dd($th);
+        $request->session()->flash('alert', [
+            'type' => 'error',
+            'message' => 'Silahkan periksa kembali email dan password Anda.',
+        ]);
     }
+    
+    // Jika login gagal, redirect ke halaman login
+    return redirect('login');
+}
+
     
     public function logout(Request $request)
     {
-        Session::flush();
+         // Simpan URL saat ini sebelum logout
+         
+         Session::flush();
+         Session::put('previousUrl', url()->previous());
         // $this->guard()->logout();
         return redirect('login');
 
